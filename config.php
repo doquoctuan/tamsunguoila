f419dc1d7de1bc9145107824ed24cc35863f81b0
doquoctuan <63587709+doquoctuan@users.noreply.github.com>
2020-08-20 17:52:05 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,12 @@
 <?php
 $accessToken = "EAAEvKnvwHXgBAG5kuxYMnqOT0EoPmHzDXtZBzshA1YhwayKrIdGDcx20aMG4ZBYLL6FEPnpsh8iT1C12kACQFsxJ6y0iSmyvldk51GFhTYltqcTcUhngbHj1YoZCETn5RCOA3KrawvjwdCEyxt4eulZAUVBZAVrOwq5zo324hzAZDZD";  // PLACE YOUR FANPAGE'S ACCESS TOKEN HERE
 
-$conn = new mysqli('otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'u3zuylg51m7x01eu', 'a6xhckoto8haoj3s', 'qnymtzcf5hep13df');
+$DBHOST = getenv('DBHOST'); // Set your database host here
+$DBUSER = getenv('DBUSER'); // Set your database user here
+$DBNAME = getenv('DBNAME'); // Set your database name here
+$DBPW =  getenv('DBPASS'); // Set your database password here
+
+$conn = mysqli_connect($DBHOST, $DBUSER, $DBPW, $DBNAME); // kết nối data
 if (!$conn) {
 	die("Cannot establish connection to database.");
 }
