277bc2f7a0f270775f332c62663e758dd2a291d2
Xin chao <doquoctuan28@gmail.com>
2020-08-20 17:10:40 +0700

    sd

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $accessToken = "EAAEvKnvwHXgBAG5kuxYMnqOT0EoPmHzDXtZBzshA1YhwayKrIdGDcx20aMG4ZBYLL6FEPnpsh8iT1C12kACQFsxJ6y0iSmyvldk51GFhTYltqcTcUhngbHj1YoZCETn5RCOA3KrawvjwdCEyxt4eulZAUVBZAVrOwq5zo324hzAZDZD";  // PLACE YOUR FANPAGE'S ACCESS TOKEN HERE
 
 
-$conn = new mysqli('localhost', 'root', '', 'chatvoinguoila');
+$conn = new mysqli('otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'u3zuylg51m7x01eu', 'a6xhckoto8haoj3s', 'qnymtzcf5hep13df');
 if (!$conn) {
 	die("Cannot establish connection to database.");
 }
