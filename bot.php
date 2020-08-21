8026097ac1fae785b1b27dde7f5009c74e474eea
doquoctuan <63587709+doquoctuan@users.noreply.github.com>
2020-08-21 14:15:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
-// include "ChatFramework/autoload.php";
-// include "config.php";
+include "ChatFramework/autoload.php";
+include "config.php";
 
 $conn = new mysqli('otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','u3zuylg51m7x01eu','a6xhckoto8haoj3s','qnymtzcf5hep13df');
 if (!$conn) {
