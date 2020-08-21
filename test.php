9954e57063aa2bf987df0c00dd637abe874993d2
doquoctuan <63587709+doquoctuan@users.noreply.github.com>
2020-08-21 14:03:40 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,9 @@
 <?php
-    echo "Hello World!";
+    $conn = new mysqli('otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','u3zuylg51m7x01eu','a6xhckoto8haoj3s','qnymtzcf5hep13df');
+    header("Content-type: text/html; charset=utf-8");
+    mysqli_set_charset($conn, 'UTF8');
+
+    if(!$conn){
+        echo ("Loi");
+    } else echo("thanh cong");
 ?>
