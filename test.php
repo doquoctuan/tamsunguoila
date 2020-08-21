d7f12cda6f408ae0c1cd46fb4f0126496e8c72c9
doquoctuan <63587709+doquoctuan@users.noreply.github.com>
2020-08-21 14:13:36 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-  //  include "config.php";
+    include "config.php";
     header("Content-type: text/html; charset=utf-8");
     mysqli_set_charset($conn, 'UTF8');
     $conn->query("INSERT INTO `users` (`name`, `mess_id`, `state`) VALUES ('T3s', '2589632', '0')");
