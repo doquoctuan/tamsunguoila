a0e61829e21ce204f50510714e81da6787ac7c55
doquoctuan <63587709+doquoctuan@users.noreply.github.com>
2020-08-21 14:10:50 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
-    $conn = new mysqli('otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','u3zuylg51m7x01eu','a6xhckoto8haoj3s','qnymtzcf5hep13df');
+    include "config.php";
     header("Content-type: text/html; charset=utf-8");
     mysqli_set_charset($conn, 'UTF8');
-    $conn->query("INSERT INTO `users` (`name`, `mess_id`, `state`) VALUES ('Test nè', '253345654', '0')");
+    $conn->query("INSERT INTO `users` (`name`, `mess_id`, `state`) VALUES ('T2s', '2533455555654', '0')");
     if(!$conn){
         echo ("Loi");
     } else echo("thanh cong");
