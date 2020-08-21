c924b3ce96c868e2ad61aebe07da9801cd10c9b0
doquoctuan <63587709+doquoctuan@users.noreply.github.com>
2020-08-21 14:07:31 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli('cbetxkdyhwsb.us-east-1.rds.amazonaws.com','u3zuylg51m7x01eu','a6xhckoto8haoj3s','qnymtzcf5hep13df');
+    $conn = new mysqli('otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','u3zuylg51m7x01eu','a6xhckoto8haoj3s','qnymtzcf5hep13df');
     header("Content-type: text/html; charset=utf-8");
     mysqli_set_charset($conn, 'UTF8');
 
