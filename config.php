224a278d48f5719839be20545748afa351d0574f
doquoctuan <63587709+doquoctuan@users.noreply.github.com>
2020-08-22 09:58:32 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$accessToken = "EAAEvKnvwHXgBAFJtGJiUWDLNQA6NA20CBC9APHd5Y9bdxE1kV1gZBQBN43MdvOZA0TK8Mp1Oh3yxF2nH82pddB5gpj9QplPeYP9TIoet1BbQgPw8dLoL28dDfgEZBoeB2fgLDcK3SGqvdUZClrCbDDw5PTNy9j0yErAwqWlzFAZDZD";  // PLACE YOUR FANPAGE'S ACCESS TOKEN HERE
+$accessToken = "EAAEvKnvwHXgBAItqbOdBvdURZBkOYQNYK2YFIwIkaPLgzBMfOFesCZCBGTPHEvKR9NtBxgdsZAkNuOIdQqs1nQKMuyDIWQkmW3oTKKxwmFrQGvWr4UYTH2pCxkh3c1qfs5P0szZAlJ4EzK4mHr7vYCXNQWTcKZAQO2H7kLavtFgZDZD";  // PLACE YOUR FANPAGE'S ACCESS TOKEN HERE
 
 
 $conn = new mysqli('otwsl2e23jrxcqvx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','u3zuylg51m7x01eu','a6xhckoto8haoj3s','qnymtzcf5hep13df');
