c911b584b0a6c8c48860a020c0532a57d9bcc641
doquoctuan <63587709+doquoctuan@users.noreply.github.com>
2020-08-22 16:32:52 +0700

    Update static_menu.php

--- events/static_menu.php
+++ events/static_menu.php
@@ -3,41 +3,41 @@ $choice = $payload['choice'];
 if ($choice == "show_menu") {
 	if ($user['state'] == 0) {
 		// currently have no action
-				$firstButton = $builder->createButton("postback", "👨 Male", json_encode(array(
+				$firstButton = $builder->createButton("postback", "👨 Nam", json_encode(array(
 		"event" => "main_menu",
 		"choice" => "option_nam"
 		)));
-		$secondButton = $builder->createButton("postback", "👩 Female", json_encode(array(
+		$secondButton = $builder->createButton("postback", "👩 Nữ", json_encode(array(
 		"event" => "main_menu",
 		"choice" => "option_nu"
 		)));
-		$menu = $builder->createButtonTemplate("Choose the gender you want to connect:", [
+		$menu = $builder->createButtonTemplate("Chọn giới tính muốn ghép cặp", [
 		$firstButton,
 		$secondButton,
 	]);
 	$bot->sendMessage($userId, $menu);
 	} else if ($user['state'] == 1) {
 		// currently waiting for other participant
-		$firstButton = $builder->createButton("postback", "Exit", json_encode(array(
+		$firstButton = $builder->createButton("postback", "Thoát hàng đợi", json_encode(array(
 			"event" => "main_menu",
 			"choice" => "cancel_find_friend"
 		)));
-		$menu = $builder->createButtonTemplate("❗ Looking for the right gender, do you want to exit?", [
+		$menu = $builder->createButtonTemplate("❗ Đang trong hàng đợi. Bạn có muốn thoát hàng đợi không?", [
 		$firstButton,
 	]);
 			$bot->sendMessage($userId, $menu);
 	} else {
 		// currently in conversation
-		$firstButton = $builder->createButton("postback", "Exit", json_encode(array(
+		$firstButton = $builder->createButton("postback", "Thoát trò chuyện", json_encode(array(
 			"event" => "main_menu",
 			"choice" => "quit_conversation"
 		)));
-		$menu = $builder->createButtonTemplate("❗ During a chat, do you want to exit?", [
+		$menu = $builder->createButtonTemplate("❗ Đang trong cuộc trò chuyện. Bạn có muốn thoát trò chuyện không?", [
 		$firstButton,
 	]);
 	$bot->sendMessage($userId, $menu);
 	}
-} elseif ($choice == "show_about") {
+} else if ($choice == "tra_cuu") {
 		
 } else {
 	
